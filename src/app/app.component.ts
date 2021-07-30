c3f976db6f5f762ed708ff64ca5fe002519a1b1e
Hiep Tran <hiep.tv@sutrixsolutions.com>
2021-07-30 16:00:07 +0700

    :fire: remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,30 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'patient-search';
-  column_to_search:any;
-  searchkey:any;
-  constructor( ) {
-  }
-  getPatientData(){
-  
-  this.column_to_search = {
-      itemsPerPage: 10000,
-      order: 'DESC'
-    }
-    if(this.searchkey !== undefined && this.searchkey !== null){
-      
-      this.column_to_search.searchkey = this.searchkey;
-  }
-  
-    //In this i will cal patient data api with serach key word and i will be pass value here 
-    this.patientdata(this.column_to_search);
-    
-    
-  }
-  
-  patientdata(i: any){
-  //Here i will cal list of patients data API code
-  }
-}
+export class AppComponent {}
